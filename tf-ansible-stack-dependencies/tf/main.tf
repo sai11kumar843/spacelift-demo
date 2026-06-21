e8bdf1883132c2e7c1910837a78fdc0c1f3ce031
sai11kumar843 <allasaikumar441@gmail.com>
2026-06-21 11:47:45 +0530

    Change instance type from t2.micro to t3.micro

--- tf-ansible-stack-dependencies/tf/main.tf
+++ tf-ansible-stack-dependencies/tf/main.tf
@@ -26,19 +26,19 @@ locals {
   instances = {
     instance1 = {
       ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     instance2 = {
       ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     instance3 = {
       ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
     instance4 = {
       ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
+      instance_type = "t3.micro"
     }
   }
 }
